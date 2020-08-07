cd1c48f78e1a410683fa409f318250e7842559d8
ljil <randomlycoding@gmail.com>
2020-08-07 10:57:04 +0400

    Обновлены настройки

--- djangoGrapheneGraphql/djangoGrapheneGraphql/settings.py
+++ djangoGrapheneGraphql/djangoGrapheneGraphql/settings.py
@@ -38,12 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'graphene_django',
 
     'employee',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,5 @@ STATIC_URL = '/static/'
 GRAPHENE = {
     "SCHEMA": 'employee.schema.schema'
 }
+
+CORS_ORIGIN_ALLOW_ALL = True
